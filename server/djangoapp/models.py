3e669d71c92537a2c406298ec6075616bd904266
aamemara <121405693+aamemara@users.noreply.github.com>
2023-01-31 14:04:12 +0200

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,7 +9,11 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
-
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=10)
+    desc = models.TextField()
+    def __str__(self):
+        return self.name + ": " + self.desc
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -19,6 +23,22 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    SEDAN = 'Sedan'
+    SUV = 'SUV'
+    WAGON = 'WAGON'
+    CAR_CHOICES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'WAGON'),
+    ]
+    car_make = models.ForeignKey(CarMake, null=True, on_delete=models.CASCADE)
+    dealer_id = models.IntegerField()
+    name = models.CharField(null=False, max_length=10)
+    ctype = models.CharField(null=False, max_length=5,choices=CAR_CHOICES,default=SEDAN)
+    year = models.DateField(null=True)
+    def __str__(self):
+        return self.name + " is of type " + self.ctype + " and released on " + str(self.year)
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
